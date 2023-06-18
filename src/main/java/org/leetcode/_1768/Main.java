37a8905aa47f2b9ee4dc0c967f8dc23de34c1f1b
AskarSap <sapasask@gmail.com>
2023-06-18 19:39:52 +0300

    update_1768

--- src/main/java/org/leetcode/_1768/Main.java
+++ src/main/java/org/leetcode/_1768/Main.java
@@ -5,10 +5,12 @@ public class Main {
         String word1 = "ab";
         String word2 = "pqrs";
         String s = mergeAlternately(word1, word2);
+        String s1 = fasterMergeAlternately(word1, word2);
         System.out.println(s);
+        System.out.println(s1);
     }
 
-    public static String mergeAlternately(String word1, String word2) {
+    private static String mergeAlternately(String word1, String word2) {
         StringBuilder sb = new StringBuilder();
         char[] word1Array = word1.toCharArray();
         char[] word2Array = word2.toCharArray();
@@ -20,6 +22,21 @@ public class Main {
             if (i < word2Array.length && word2Array[i] != '\0'  ) {
                 sb.append(word2Array[i]);
             }
+        }
+        return sb.toString();
+    }
+
+    private static String fasterMergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < word1.length() || i < word2.length()) {
+            if (i < word1.length()) {
+                sb.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                sb.append(word2.charAt(i));
+            }
+            i++;
         }
         return sb.toString();
     }
