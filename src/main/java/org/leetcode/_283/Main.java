662e0e5f4e1c1e40c43d8ebdf3be6218fd1be3ec
AskarSap <sapasask@gmail.com>
2023-06-23 14:13:15 +0300

    238

---
+++ src/main/java/org/leetcode/_283/Main.java
@@ -0,0 +1,22 @@
+package org.leetcode._283;
+
+public class Main {
+  public static void main(String[] args) {
+    moveZeroes(new int[]{0, 1, 0, 3, 12});
+  }
+
+  public static void moveZeroes(int[] nums) {
+    int pos = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != 0) {
+        if (i != pos) {
+          nums[pos] = nums[i];
+          nums[i] = 0;
+        }
+        pos++;
+      }
+    }
+
+  }
+
+}
--- src/main/java/org/leetcode/_443/Main.java
+++ src/main/java/org/leetcode/_443/Main.java
@@ -9,16 +9,15 @@ public class Main {
   private static int compress(char[] chars) {
     int index = 0;
     int i = 0;
-    while (i < chars.length) {
+    while ( i < chars.length) {
       int j = i;
-      while (j < chars.length && chars[i] == chars[j]) {
+      while (j < chars.length && chars[j] == chars[i]) {
         j++;
       }
-
       chars[index++] = chars[i];
       if (j - i > 1) {
         String count = j - i + "";
-        for (char c : count.toCharArray()) {
+        for ( char c : count.toCharArray()) {
           chars[index++] = c;
         }
       }
