9e1561af06fbce1298d3ead6369636b331ed58cf
AskarSap <sapasask@gmail.com>
2023-06-25 17:48:08 +0300

    11

---
+++ src/main/java/org/leetcode/_11/Main.java
@@ -0,0 +1,24 @@
+package org.leetcode._11;
+
+public class Main {
+  public static void main(String[] args) {
+
+  }
+
+  private static int maxArea(int[] height) {
+    int pointer1 = 0;
+    int pointer2 = height.length - 1;
+    int maxArea = 0;
+    while (pointer1 < pointer2) {
+      if (height[pointer1] < height[pointer2]) {
+        maxArea = Math.max(maxArea, height[pointer1] * ( pointer2 - pointer1));
+        pointer1 += 1;
+      } else {
+        maxArea = Math.max(maxArea, height[pointer2] * (pointer2 - pointer1));
+        pointer2 -= 1;
+      }
+    }
+    return maxArea;
+  }
+
+}
