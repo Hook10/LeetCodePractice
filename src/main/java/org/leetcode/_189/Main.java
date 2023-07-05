5ae21f97f7f3abb529cdddbe78a1a4c9c958ac3f
AskarSap <sapasask@gmail.com>
2023-07-05 10:43:22 +0300

    189 rotate array

--- src/main/java/org/leetcode/_189/Main.java
+++ src/main/java/org/leetcode/_189/Main.java
@@ -7,8 +7,8 @@ public class Main {
     int[] nums = {1, 2, 3, 4, 5, 6, 7};
     int[] nums2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    rotateArray(nums, 3);
-    rotateArray(nums2, 5);
+    rotate(nums, 3);
+    rotate(nums2, 5);
 
     System.out.println(Arrays.toString(nums));
     System.out.println(Arrays.toString(nums2));
@@ -18,15 +18,14 @@ public class Main {
   // 987654321 first step reverse whole array
   // 789 654321 reverse part by k
   // 789 123456 reverse part after k
-  private static void rotateArray(int[] nums, int k) {
+  private static void rotate(int[] nums, int k) {
     k = k % nums.length;
     reverse(nums, 0, nums.length - 1);
     reverse(nums, 0, k - 1);
     reverse(nums, k, nums.length - 1);
-
   }
 
-  public static void reverse(int[] nums, int start, int end) {
+  private static void reverse(int[] nums, int start, int end) {
     while (start < end) {
       int temp = nums[start];
       nums[start] = nums[end];
@@ -35,4 +34,6 @@ public class Main {
       end--;
     }
   }
+
+
 }
