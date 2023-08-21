f29b94a74137af87374440d79a6c714be7212c28
AskarSap <sapasask@gmail.com>
2023-08-21 23:29:21 +0600

    8 String to Integer (atoi)

---
+++ src/main/java/org/leetcode/_8/Main.java
@@ -0,0 +1,37 @@
+package org.leetcode._8;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(myAtoi("42"));
+    System.out.println(myAtoi("4193 with words"));
+    System.out.println(myAtoi("008"));
+  }
+
+  private static int myAtoi(String s) {
+    if (s.isEmpty()) {
+      return 0;
+    }
+    int res = 0, i = 0, sign = 1;
+
+    while (i < s.length() && s.charAt(i) == ' ') {
+      i++;
+    }
+
+    if (i < s.length() && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+      if (s.charAt(i++) == '-') {
+        sign = -1;
+      }
+    }
+    while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+      if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && s.charAt(i) - '0' > 7)) {
+        if (sign == -1) {
+          return Integer.MIN_VALUE;
+        }
+        return Integer.MAX_VALUE;
+      }
+      res = res * 10 + (s.charAt(i++) - '0');
+    }
+    return sign * res;
+  }
+
+}
