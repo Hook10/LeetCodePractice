b374920bd2ea346e5d7f6155c138ff498b6baf82
AskarSap <sapasask@gmail.com>
2024-03-28 12:53:31 +0500

    283 Move zeroes

--- src/main/java/org/leetcode/_283/Main.java
+++ src/main/java/org/leetcode/_283/Main.java
@@ -1,8 +1,12 @@
 package org.leetcode._283;
 
+import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args) {
-    moveZeroes(new int[]{0, 1, 0, 3, 12});
+    int[] nums = {0, 1, 0, 3, 12};
+    moveZeroes(nums);
+    System.out.println(Arrays.toString(nums));
   }
 
   public static void moveZeroes(int[] nums) {
@@ -12,8 +16,8 @@ public class Main {
         if (i != pos) {
           nums[pos] = nums[i];
           nums[i] = 0;
-          pos++;
         }
+        pos++;
       }
     }
   }
